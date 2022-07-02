869f4aa2d612fe4e4291d45b6a5027044e3aa09d
selcukemreozer <selcuk.emre.ozer@outlook.com>
2022-07-02 04:42:24 +0300

    RAKES first steps and github connect

--- RAKES.py
+++ RAKES.py
@@ -1,11 +1,35 @@
 from bs4 import BeautifulSoup
 import requests as rq
+import random as rd
 
 URL = "https://github.com/selcukemreozer/English-Playground/blob/main/kelime_bankalari/a1.txt"
 page = rq.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.findAll("td", {"id":"LC1"})
-print(results)
-list_ = list()
 
+results = str(results)
+
+for each in range(len(results)): # gereksiz html parçalarını ayıklıyor ve sadece kelimeler kalıyor.
+    # (Bilmiyorum sanki daha mantıklı ve kullanışlı bir yolu varmış gibi geliyor.)
+
+    if results[each] == ">":
+        sonuc = results[each:]
+
+        for each2 in range(len(results[each:])):
+
+            if results[each2+each] == "<":
+                kelimeler = results[each+1:each2+each]
+                break
+        break
+
+print(kelimeler)
+
+list_ = kelimeler.split(",") # burada kelimeler ve karşılıkları gruplara ayrılıyor ve listede tutuluyor.
+                             # Ör.["apple:elma", "car:araba"]
+secim = rd.choice(list_)     # rastgele bir kelime veanlamı seçiliyor
+
+cevap = input(secim.split(":")[0]+">") # seçilen kelime gösterilip anlamı soruluyor
+
+if cevap == secim.split(":")[1]:
+    print("doğru")
--- githubCommunityContent.py
+++ githubCommunityContent.py
@@ -1,19 +1,21 @@
-from github import Github
+from github import Github # https://pygithub.readthedocs.io/en/latest/introduction.html
 
-access_token = "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"
+access_token = "xxxx" # 9 Temmuza kadar geçerli sadece açık repolara erişim var.
 g = Github(access_token)
 
 # user = g.get_user("selcukemreozer")
-repo = g.get_repo("selcukemreozer/English-Playground")
+
 """
 repo.create_file("test.txt", "başlık", "içerik", branch="community")
 
 # community branchi bu program için oluşturulmuş bir branchdir.
 # başlık ve dosya adı program tarafından numaralandırılabilir. Kimlik atamak gibi.
 # içerik kısmında kullanıcıların oluşturduğu kelime bankaları olacak.(sadece string tipinde değişken kabul ediyor)
-
 """
+def KELIMEBANKASI_EKLE(dosyaAdi, baslik, icerik, branchName):
 
+    repo = g.get_repo("selcukemreozer/English-Playground")
+    repo.create_file(dosyaAdi, baslik, icerik, branch=branchName)
 
 
 
